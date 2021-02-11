78b21c4684d5bcc01767c8e66a1bdea365b25962
Adriano Todeschini <77498948+Adriano-Todeschini@users.noreply.github.com>
2021-02-10 22:45:13 -0300

    Removido index

--- index.js
+++
@@ -1,9 +0,0 @@
-alert("Olá! Esse é o meu primeiro comando JavAScript!");
-
-var remetente = prompt('Qual o nome do remetente?');
-var destinatário = prompt('Qual o nome do destinatário?');
-var mensagem = prompt('Digite sua mensagem');
-
-alert('Remetente: ' + remetente);
-alert('Destinatário: ' + destinatário);
-alert('Mesangem: ' + mensagem);
